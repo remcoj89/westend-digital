8dbdfbebd6b0dc88f070e7ff2f12ae79f59c5fd7
remco jonk <remcojonk@hotmail.com>
2024-12-17 15:09:16 +0100

    new build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
     site: 'https://westend-digital.nl/',
     output: 'server',
-    adapter: netlify(),
+    adapter: netlify({
+      imageCDN: false,
+    }),
     image: {
       domains: ['https://mediumturquoise-sparrow-543006.hostingersite.com/']
     },
