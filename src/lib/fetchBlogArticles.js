ef34224f463e3db0ca530fdb42ea04cc3c74069f
remco jonk <remcojonk@hotmail.com>
2025-01-03 14:47:51 +0100

    fetching blog data

---
+++ src/lib/fetchBlogArticles.js
@@ -0,0 +1,39 @@
+export default async function fetchBlogArticles() {
+  const query = `
+    query GetlBlog {
+      posts(where: {categoryId: 1}) {
+        edges {
+          node {
+            id
+            slug
+            title
+            content(format: RENDERED)
+            featuredImage {
+              node {
+                altText
+                mediaItemUrl
+                description(format: RENDERED)
+              }
+            }
+          }
+        }
+      }
+    }
+  `
+  const res = await fetch(import.meta.env.WP_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({ query }),
+  })
+
+
+  if (!res.ok) {
+    throw new Error(`Network response was not ok: ${res.statusText}`);
+  }
+
+  const { data } = await res.json() // Destructure the data from the response
+
+  return data.posts.edges // Return the posts from the response
+}
