422df2fc98fef27e791fbdde7ba9c72e6c15c0dd
remcojonk <remcojonk@hotmail.com>
2025-09-10 15:51:29 +0200

    added faq's and testemonial section to website page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,22 @@ import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://westend-digital.nl/',
-  output: 'static',
-  adapter:  netlify(),
+  site: "https://westend-digital.nl/",
+  output: "static",
+  adapter: netlify(),
   image: {
-    domains: ['https://mediumturquoise-sparrow-543006.hostingersite.com/']
+    domains: [
+      "https://mediumturquoise-sparrow-543006.hostingersite.com/",
+      "https://lh3.googleusercontent.com",
+    ],
   },
   integrations: [
     react(),
     sitemap({
-      changefreq: 'weekly',
+      changefreq: "weekly",
       priority: 0.8,
       lastmod: new Date(),
-    })
+    }),
   ],
   experimental: {
     // svg: true,
